cc7d23054922bf272465212d3730dd1a1fcbda81
Ryan Beltz <ryanbeltzbackup@gmail.com>
2015-02-12 16:19:55 -0500

    Create example.rb

---
+++ example.rb
@@ -0,0 +1,10 @@
+require_relative 'engine'
+test = Engine.new
+test.title("Test game")
+puts "Welcome to my game"
+test.continue
+puts "I'm about to clear the screen"
+test.continue
+test.clear
+puts  "Cleared!"
+test.continue
